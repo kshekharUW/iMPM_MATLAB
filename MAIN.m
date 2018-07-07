ff378080a35a2c61d08d64f1a4939a1624a4d134
kshekharUW <kshekhar@uw.edu>
2018-07-06 20:37:55 -0700

    Applied BCs to vstar and fictitious velocity for initialization.

--- MAIN.m
+++ MAIN.m
@@ -3,7 +3,7 @@ clc; clear; close all;
 nel=[8,8]; npe=[2,2];
 nx=nel+1;
 xmin=[0,0];xmax=[1,1];
-Tmax =10.5;
+Tmax =100.0;
 % get phase setup info
 [ identifier,Number_of_regions,xcenter,box_inner,box_outer,Hwater,v_initial,rho_initial,mu,K,E,lambda,material,gravity ] = ...
     ipTest();
@@ -12,13 +12,12 @@ rhoWater=1000;
 % initialize the grid
 [dx,Nn,Ne,x,icon,numOC,ntop,nbot,nright,nleft,freeDOFs]=initgrid(nx,xmin,xmax);
 lx=dx(1); ly=dx(2);
-gp=1/(sqrt(3))*[-1,1,1,-1;
-    -1,-1,1,1];  % Gauss points
 
 % assemble matrices
 [L,C,DxM,DyM,G,M,DivM,detJ]=assemble(dx,Nn,Ne,icon);
             
 % iniitalize the material points
+
 [np,xp,xp0,vp,rp,Fp,rhop,rho0p,sigp,pp,mp,vol_dil,MP_tag]= ...
     initpar(npe,dx,icon,x,Ne,Number_of_regions,xcenter,box_inner,box_outer,v_initial,rho_initial,identifier);
 
@@ -27,28 +26,36 @@ gp=1/(sqrt(3))*[-1,1,1,-1;
 ntopright=[];
 
 % time stepping
-CFL = 0.05;
-dt0 = CFL * min(dx(1),dx(2))/1;
-t=0; counter = 0; dt=dt0;
+CFL = 0.1;
+dt = CFL * min(lx,ly)/1;
+t=0;
+Tmax=Tmax+dt;
+
+% Flow parameters
+Re=1;
+mu=rhoWater/Re;
 
 % calculate the natural coordinates
 [xpn,nep]=natcoords(xp,dx,xmin,nx-1);
 % map material point data to the grid
 [mv,v]=maptogridIC(Nn,Ne,icon,xpn,dx,dt,nep,np,mp,vp,sigp,pp,rho0p,gravity,material,freeDOFs,nbot,ntop,nleft,nright,ntopright);
-nodePressure= zeros(Nn,1);
-b=zeros(2*Nn,1); 
-vstar=vBC; 
-vnew =zeros(2*Nn,1); 
+ 
+% Fictitious time steps to determine initial state
+[nodePressure]=solveP(L,DivM,vBC,qn,pSupportDOF,rhoWater,1.0);
+[vnew]=solveVtilda(nodePressure,Nn,nbot,ntop,nleft,nright,nx,lx,ly,rhoWater,1.0);
+vnew(vSupportDOF) = vBC(vSupportDOF);
+%
 
 picCounter=0;
-% load ic4x4_t200;
-% vstar=vnew;
-Re=1;
-mu=rhoWater/Re;
-% dtSwitch=0;
-t=t-dt;
 while(t<=Tmax)    
-    t=t+dt; fprintf('time=%f\n',t);    
+    fprintf('time=%f\n',t);   
+    
+    %%% Add viscosity forces using gauss pt integration
+    [force]=getForce(mu,Nn,Ne,icon,lx,ly,vnew,mv);
+    accn = force./[mv;mv];    
+    vstar=vnew + accn*dt;
+    vstar(vSupportDOF) = vBC(vSupportDOF);
+    %%% 
 
     %%% Solve pressure Poisson's equation 
     [nodePressure]=solveP(L,DivM,vstar,qn,pSupportDOF,rhoWater,dt);
@@ -63,27 +70,22 @@ while(t<=Tmax)
     %%% plotting and data storage
     storeInterval=dt;    
     if (mod(t-dt,storeInterval)<1e-8 || storeInterval-mod(t-dt,storeInterval)<1e-8)
-        picCounter=picCounter+1;
         plotProj( 1,Nn,xp,vp,x,sigp,xmin,xmax,t-dt,nx,nodePressure,vnew,picCounter,nbot,ntop,nleft,nright,0,0 );
+        picCounter=picCounter+1;
     end
     %%% end
 
-    %%% Map to points
-    [xp,vp,particlePressure]=maptopointsPC(Nn,xmin,nx,np,xp,vp,icon,vnew,v,nodePressure,dx,dt,1);
-    %%%% end
+%     %%% Map to points
+%     [xp,vp,particlePressure]=maptopointsPC(Nn,xmin,nx,np,xp,vp,icon,vnew,v,nodePressure,dx,dt,1);
+%     %%%% end
     
 %     %%% Map to grid
 %     [xpn,nep]=natcoords(xp,dx,xmin,nx-1);
 %     [mv,vnew]=maptogridIC(Nn,Ne,icon,xpn,dx,dt,nep,np,mp,vp,sigp,pp,rho0p,gravity,material,freeDOFs,nbot,ntop,nleft,nright,ntopright);
 %     vnew(vSupportDOF) = vBC(vSupportDOF);
 %     %%% end
-    
-    %%% Add viscosity forces using gauss pt integration
-    [force]=getForce(mu,Nn,Ne,icon,lx,ly,vnew,mv);
-    accn = force./[mv;mv];    
-    %%% 
-
-    v=vnew;
-    vstar=vnew + accn*dt;
+  
+    v=vnew;    
+    t=t+dt;
 end
 
--- getForce.m
+++ getForce.m
@@ -9,14 +9,15 @@ function [force]=getForce(mu,Nn,Ne,icon,lx,ly,vnew,mv)
         
         A = (0.5/ly)*(vnew(iv(1)+Nn) - vnew(iv(2)+Nn) + vnew(iv(3)+Nn) - vnew(iv(4)+Nn));
         B = (0.5/lx)*(vnew(iv(1))    - vnew(iv(2))    + vnew(iv(3))    - vnew(iv(4)));
+        fcell = zeros(4,2);
         for j=1:4 % loop over 4 gauss points
             ss=gp(1,j);tt=gp(2,j);
-            gradN = getGrad([ss,tt]);
+            gradN = getGrad([ss,tt],lx,ly);
             
-            gv(1,1)=2/lx*gradN(1,:)*vnew(iv);   % velocity gradient 
-            gv(1,2)=2/ly*gradN(2,:)*vnew(iv);   % velocity gradient 
-            gv(2,2)=2/ly*gradN(2,:)*vnew(iv+Nn);% velocity gradient 
-            gv(2,1)=2/lx*gradN(1,:)*vnew(iv+Nn);% velocity gradient 
+            gv(1,1)=gradN(1,:)*vnew(iv);   % velocity gradient 
+            gv(1,2)=gradN(2,:)*vnew(iv);   % velocity gradient 
+            gv(2,2)=gradN(2,:)*vnew(iv+Nn);% velocity gradient 
+            gv(2,1)=gradN(1,:)*vnew(iv+Nn);% velocity gradient 
             
             d      = 0.5*(gv+gv');          % rate of strain at gauss point j
             ddev   = d-1/3*trace(d)*eye(2); % rate of shear at gauss point j             
@@ -26,11 +27,8 @@ function [force]=getForce(mu,Nn,Ne,icon,lx,ly,vnew,mv)
             dhat(1,2)= 0; dhat(2,1)=0;       % enhanced velocity contributions
             
             tau      = 2*mu*(ddev); 
-            fgp      = tau*gradN*lx*ly/4;
-            
-%             accn(iv)   = accn(iv)   - fgp(1,:)'./mv(iv);
-%             accn(iv+Nn)= accn(iv+Nn)- fgp(2,:)'./mv(iv);
-            force(iv)   = force(iv)   - fgp(1,:)';
-            force(iv+Nn)= force(iv+Nn)- fgp(2,:)';            
+            fcell      = fcell + (tau*gradN*lx*ly/4)';   
         end
+        force(iv)   = force(iv)   - fcell(:,1);
+        force(iv+Nn)= force(iv+Nn)- fcell(:,2);
     end
